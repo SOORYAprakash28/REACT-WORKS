ab6fb6a4e9ab970733fd4c559032f39df747b816
SOORYA PRAKASH ACHARYA <sooryapacharya@gmail.com>
2024-09-16 10:06:54 +0530

    simple practices

---
+++ es-arrays.js
@@ -0,0 +1,12 @@
+let numbers=[1,2,3,4,5,6,7,8,9];
+let names=['soorya','prakash','acharya'];
+console.log(numbers,names);
+
+// for each function for each element in the arrya
+
+numbers.forEach(element => console.log(element));
+
+// filter ourt the elements that donot matc hthe condition
+
+let evennumbers = numbers.filter(num => num%2 == 0);
+console.log(evennumbers);
---
+++ es-names.js
@@ -0,0 +1,3 @@
+let names=['soorya','shreyas','sumanth','nitin','vishnu','shivaneeth','preethesh'];
+let namewith6 = names.filter(name => name.length >=6);
+console.log(namewith6);
