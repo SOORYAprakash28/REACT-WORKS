724ebab010eb81b8167b7b869478ced1120655ac
SOORYA PRAKASH ACHARYA <sooryapacharya@gmail.com>
2024-09-16 12:35:00 +0530

    simple practices

--- es-destucturing.js
+++ es-destucturing.js
@@ -8,3 +8,34 @@ const [firsttask,secondtask, thirdtask]=todo;
 console.log(firsttask);
 console.log(secondtask);
 console.log(thirdtask);
+
+
+const product = {name:"laptop",brand:"HP",price:"50,000"};
+const {name,brand,price}=product;
+console.log(name);
+console.log(brand);
+console.log(price);
+
+
+// destucting nested objectr and array
+const response={
+    status:200,
+    data:{
+        user:{
+            usname:"soorya",
+            address:{
+                city:"padubidri",
+                country:"India",
+            }
+        }
+    }
+}
+
+const {data:{user:{usname,address:{city,country}}}}=response;
+console.log(usname);
+console.log(city);
+console.log(country);
+
+
+
+
---
+++ es-spread.js
@@ -0,0 +1,5 @@
+const electronics=["laptop", "Smartphone","smartwatch"];
+const groceris = ['apple','mango'];
+const cart=[...electronics,...groceris];
+console.log(cart);
+
